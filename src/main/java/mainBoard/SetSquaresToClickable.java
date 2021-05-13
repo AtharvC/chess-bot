38b33f33d7cea0b3f878906c1320e1e71d43c9f9
Yuicchi-chan <pawar.progamer05@gmail.com>
2021-05-13 21:46:45 +0530

    Fixed @PoopySheep's code where it allowed clicking on empty cells

--- src/main/java/mainBoard/SetSquaresToClickable.java
+++ src/main/java/mainBoard/SetSquaresToClickable.java
@@ -1,47 +1,43 @@
 package mainBoard;
 
 import javax.swing.*;
-import java.awt.event.*;
-
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
 
 
 public class SetSquaresToClickable {
 
-    InitializeBoard initializeBoard=new InitializeBoard();
-    JPanel[][] squares=initializeBoard.getSquares();
+
+    InitializeBoard initializeBoard = new InitializeBoard();
+    JPanel[][] squares = initializeBoard.getSquares();
 
 
-    SetSquaresToClickable(){
-        for(int i=0;i<8;i++)
-        {for(int j=0;j<8;j++)
-        {
-            if((i+j)%2==0)
-            {squares[i][j].addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    System.out.println("The white square was clicked");
+    SetSquaresToClickable() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (squares[i][j].getComponents().length > 0) {
+                    int finalI = i;
+                    int finalJ = j;
+                    squares[i][j].addMouseListener(new MouseAdapter() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            squares[finalI][finalJ].setBackground(Color.blue);
+                            System.out.println(Arrays.toString(Arrays.stream(squares[finalI][finalJ].getComponents()).toArray()));
 
+                        }
+                    });
                 }
-            });
-            }
-            else if((i+j)%2!=0)
-            {
-                squares[i][j].addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        System.out.println("The black square was clicked");
-                    }
-                });
-            }
-        }}
 
 
+            }
+        }
     }
 
 
     public static void main(String[] args) {
         new SetSquaresToClickable();
-
 
     }
 }
